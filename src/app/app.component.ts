d110622e33133445a5fb164cee0d7f54227bbf72
Alaa-Khalila <alkhalila.alaa@gmail.com>
2021-07-21 15:14:45 +0300

    added style to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,19 @@ export class AppComponent {
   data: any[] = [
     {
       imgUrl: '../assets/images/biking.jpeg',
-      title: 'mountain biking',
+      title: 'Mountain Biking',
       tag: '@biking1222',
       text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae eligendi  quia ipsam rerum asperiores minus distinctio repellat ad a aut.',
     },
     {
       imgUrl: '../assets/images/mountain.jpeg',
-      title: ' snowy mountain ',
+      title: ' Snowy Mountain ',
       tag: '@hiking person',
       text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae eligendi  quia ipsam rerum asperiores minus distinctio repellat ad a aut.',
     },
     {
       imgUrl: '../assets/images/tree.jpeg',
-      title: 'neat tree',
+      title: 'Neat Tree',
       tag: '@nature',
       text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae eligendi  quia ipsam rerum asperiores minus distinctio repellat ad a aut.',
     },
